f0d26f5a2ac7710501bbaaaaa6207f6b6c264475
mounasb <mounasb@pm.me>
2022-06-30 17:49:09 +0200

    plop

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-from dotenv import load_dotenv
-import os
 import streamlit as st
+# from dotenv import load_dotenv
+# import os
+#
+# load_dotenv()
+#
+# EMAIL = 'blabla@grrr.com'
+# PASSWORD = 'hophop'
 
-load_dotenv()
-
-EMAIL = 'blabla@grrr.com'
-PASSWORD = 'hophop'
-
+st.text("Du texte pour tester des truuuuqueuh")
 st.write(EMAIL)
 st.write(PASSWORD)
-st.text("Du texte pour tester des truuuuqueuh")
-st.write(os.getenv("EMAIL"))
-st.write(os.getenv("PASSWORD"))
+# st.write(os.getenv("EMAIL"))
+# st.write(os.getenv("PASSWORD"))
