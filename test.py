98010cd71e5ccb862cc97d38293b2fd4489a6cbd
mounasb <mounasb@pm.me>
2022-07-11 09:41:39 +0200

    code cleaning

--- test.py
+++ test.py
@@ -1,16 +1,7 @@
 import streamlit as st
-# from dotenv import load_dotenv
-# import os
-#
-# load_dotenv()
-#
-# EMAIL = 'blabla@grrr.com'
-# PASSWORD = 'hophop'
 
-st.text("Du texte pour tester des truuuuqueuh")
+st.text("Du texte pour tester des trucs")
 EMAIL = st.secrets.EMAIL
 PASSWORD = st.secrets.PASSWORD
 st.write(EMAIL)
 st.write(PASSWORD)
-# st.write(os.getenv("EMAIL"))
-# st.write(os.getenv("PASSWORD"))
